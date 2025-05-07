f70370fd9c2737e98caeea198f1a90de7425373c
VvTEvV <japangzaqasd@gmail.com>
2025-05-07 16:53:47 +0700

    add  git calculator

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title("Basic Git & Github By VvTEvV")
+GUI.geometry("500x700")
+
+L = Label(GUI,text="VvTEvV") #
+L.pack()
+
+
+GUI.mainloop()
